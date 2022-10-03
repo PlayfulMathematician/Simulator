bf7fcf0273f19cbf5aae5d0c6e61ff56379ddc35
Otis Heggem <otisheggem@gmail.com>
2022-10-03 16:18:52 -0600

    Added Frequency Meta Simulation

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ class FrequencySimulation:
 
     @property
     def frequency_data(self):
-        qt = time.perf_counter()
-        print(qt - st)
         return {j: len([i for i in self.l if i == j])/len(self.l) for j in set(self.l)}
 
 
@@ -24,4 +22,10 @@ class DiceSimulation(FrequencySimulation):
         super().__init__([sum([random.choice(faces) for q in range(0, sum_amt)]) for i in range(0, roll_amt)])
 
 
-r = FrequencySimulation([random.uniform(-1, 1)**2 + random.uniform(-1, 1)**2 < 1 for i in range(0, 10000)])
+def f():
+    return FrequencySimulation([random.uniform(-1, 1)**2 + random.uniform(-1, 1)**2 < 1 for i in range(0, 100000)])
+
+
+a = FrequencySimulation([f().frequency_data[True]*4 for i in range(0, 100)]).frequency_data
+print(sum(list(a.keys()))/100)
+print(f().frequency_data[True]*4)
