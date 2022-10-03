286c6847035fba39c59ab8f7002ee7be93626fa2
Otis Heggem <otisheggem@gmail.com>
2022-10-03 16:22:44 -0600

    Formatting

--- main.py
+++ main.py
@@ -8,22 +8,37 @@ import time
 
 class FrequencySimulation:
     """This is the frequency simulator"""
+
     """"""
+
     def __init__(self, l):
         self.l = l
 
     @property
     def frequency_data(self):
-        return {j: len([i for i in self.l if i == j])/len(self.l) for j in set(self.l)}
+        return {
+            j: len([i for i in self.l if i == j]) / len(self.l) for j in set(self.l)
+        }
 
 
 class DiceSimulation(FrequencySimulation):
     def __init__(self, faces, roll_amt, sum_amt):
-        super().__init__([sum([random.choice(faces) for q in range(0, sum_amt)]) for i in range(0, roll_amt)])
+        super().__init__(
+            [
+                sum([random.choice(faces) for q in range(0, sum_amt)])
+                for i in range(0, roll_amt)
+            ]
+        )
 
 
 def f():
-    return FrequencySimulation([random.uniform(-1, 1)**2 + random.uniform(-1, 1)**2 < 1 for i in range(0, 100000)])
+    return FrequencySimulation(
+        [
+            random.uniform(-1, 1) ** 2 + random.uniform(-1, 1) ** 2 < 1
+            for i in range(0, 100000)
+        ]
+    )
 
-print(sum([f().frequency_data[True]*4 for i in range(0, 100)])/100)
-print(f().frequency_data[True]*4)
+
+print(sum([f().frequency_data[True] * 4 for i in range(0, 100)]) / 100)
+print(f().frequency_data[True] * 4)
